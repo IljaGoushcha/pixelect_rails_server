add82d2525ea2cabae99e57b988ae7af230b2182
IljaGoushcha <iljagou@yahoo.com>
2014-12-04 18:27:24 -0500

    added root to objects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
+  @image_set = ImageSet.find(params[:id])
+  @comment = Comment.find(params[:id])
+  @tag = Tag.find(params[:id])
+  @user = User.find(params[:id])
+
+  def findObjects(Object, id)
+    return Object.find(id)
+  end
+  @image_set = findObject(ImageSet, params[:id])
 
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,8 @@ class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :image
 
-  # def get_total_likes
-  #   this.total_likes = logic
-  # end
+  def update_total_likes
+    this.total_likes = logic
+  end
 
 end
